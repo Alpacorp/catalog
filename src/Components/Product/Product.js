752f667673e9060280a696874b81816357d64156
Alpacorp <alejandro.palacios88@gmail.com>
2022-03-29 23:24:58 -0500

    add function to see products in the cart

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { formatPrice } from "../../utils/formatPrice.";
 import ModalProduct from "../ModalProduct/ModalProduct";
@@ -23,6 +23,7 @@ const Product = ({
 }) => {
   console.log("cargue de producto");
   const [modalState, setModalState] = useState(false);
+  const [exist, setExist] = useState();
   const showHide = useCallback(
     () => setModalState((modalState) => !modalState),
     []
@@ -30,6 +31,7 @@ const Product = ({
 
   const productList = "product-list";
   let recoveredData = localStorage.getItem(productList);
+  let data = JSON.parse(recoveredData);
 
   const handleAddToCart = (id, name, price, description, image, discount) => {
     const productData = [{ id, name, price, description, image, discount }];
@@ -37,12 +39,23 @@ const Product = ({
     if (recoveredData === null) {
       localStorage.setItem(productList, JSON.stringify(productData));
     } else {
-      let data = JSON.parse(recoveredData);
       let newData = { id, name, price, description, image, discount };
       data.push(newData);
       localStorage.setItem(productList, JSON.stringify(data));
     }
   };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const checkProductCart = (id) => {
+    let data = JSON.parse(recoveredData);
+    const filterData = data.some((product) => product.id === id);
+    setExist(filterData);
+    return filterData;
+  };
+
+  useEffect(() => {
+    checkProductCart(id);
+  }, [checkProductCart, exist, id]);
 
   return (
     <>
@@ -89,12 +102,13 @@ const Product = ({
         />
         <div className="add-car">
           <button
-            disabled
+            disabled={exist}
             onClick={() =>
               handleAddToCart(id, name, price, description, image, discount)
             }
+            title={!exist ? "Agregar al Carrito" : "Producto en carrito"}
           >
-            Agregar al Carrito
+            {!exist ? "Agregar al Carrito" : "Producto en carrito"}
           </button>
         </div>
       </ModalProduct>
