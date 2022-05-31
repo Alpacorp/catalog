0931040ca11f1b88b877950db9e90b7f0d2b08d7
Alpacorp <alejandro.palacios88@gmail.com>
2022-05-30 23:29:03 -0500

    initial redux implementation added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Containers/AppRoutes";
 import "./index.css";
+import { store } from "./Store/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppRoutes />
+      <Provider store={store}>
+        <AppRoutes />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
